8ee49773243345d561a929d4236011a4f6ffeef5
Viraja <palletiviraja@gmail.com>
2026-06-23 12:59:23 +0530

    Time: 7 ms (98.36%), Space: 22.1 MB (55.2%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            signature = "".join(sorted(word))
+            
+            
+            anagram_map[signature].append(word)
+            
+        return list(anagram_map.values())
